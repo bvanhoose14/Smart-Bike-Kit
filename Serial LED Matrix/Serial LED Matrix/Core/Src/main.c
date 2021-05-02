b20dbc8798bb733260dd4420135bbc0f998f80ab
BEN VAN HOOSE <u1134346@umail.utah.edu>
2021-05-02 16:27:40 -0600

    Final edit

--- Serial LED Matrix/Serial LED Matrix/Core/Src/main.c
+++ Serial LED Matrix/Serial LED Matrix/Core/Src/main.c
@@ -1,8 +1,7 @@
-/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body
+  * @brief          : Serial LED Matrix
   ******************************************************************************
   * @attention
   *
@@ -16,141 +15,93 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-uint8_t disp1ay[13][8]={
-{0x18,0xC,0x6,0x3,0x3,0x6,0xC,0x18},//>>
-{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0},//empty
-{0x18,0xC,0x6,0x3,0x3,0x6,0xC,0x18},//>>
-{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0},//empty
-{0x18,0xC,0x6,0x3,0x3,0x6,0xC,0x18},//>>
-{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0},//empty
-{0x18,0x30,0x60,0xC0,0xC0,0x60,0x30,0x18},//<<
-{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0},//empty
-{0x18,0x30,0x60,0xC0,0xC0,0x60,0x30,0x18},//<<
-{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0},//empty
-{0x18,0x30,0x60,0xC0,0xC0,0x60,0x30,0x18},//<<
-{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0},//empty
+//LED matrix display, each bit represents an LED on or off for the row 
+uint8_t disp1ay[4][8]={
+{0x18,0xC,0x6,0x3,0x3,0x6,0xC,0x18},			//right arrow >
+{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0}, 				//off
+{0x18,0x30,0x60,0xC0,0xC0,0x60,0x30,0x18},//left arrow <
+{0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0},				//off
 };
 
-//{0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C},//0
-//{0x10,0x30,0x50,0x10,0x10,0x10,0x10,0x7c},//1
-//{0x7E,0x2,0x2,0x7E,0x40,0x40,0x40,0x7E},//2
-//{0x3E,0x2,0x2,0x3E,0x2,0x2,0x3E,0x0},//3
-//{0x8,0x18,0x28,0x48,0xFE,0x8,0x8,0x8},//4
-//{0x3C,0x20,0x20,0x3C,0x4,0x4,0x3C,0x0},//5
-//{0x3C,0x20,0x20,0x3C,0x24,0x24,0x3C,0x0},//6
-//{0x3E,0x22,0x4,0x8,0x8,0x8,0x8,0x8},//7
-//{0x0,0x3E,0x22,0x22,0x3E,0x22,0x22,0x3E},//8
-//{0x3E,0x22,0x22,0x3E,0x2,0x2,0x2,0x3E},//9
-//{0x18,0x24,0x42,0x42,0x7E,0x42,0x42,0x42},//A
-//{0x3C,0x22,0x22,0x3c,0x22,0x22,0x3C,0x0},//B
-//{0x3C,0x40,0x40,0x40,0x40,0x40,0x40,0x3C},//C
-//{0x7C,0x22,0x22,0x22,0x22,0x22,0x22,0x7C},//D
-//{0x7C,0x40,0x40,0x7C,0x40,0x40,0x40,0x7C},//E
-//{0x7C,0x40,0x40,0x7C,0x40,0x40,0x40,0x40},//F
-//{0x3C,0x40,0x40,0x40,0x4c,0x44,0x44,0x3C},//G
-//{0x44,0x44,0x44,0x7C,0x44,0x44,0x44,0x44},//H
-//{0x7C,0x10,0x10,0x10,0x10,0x10,0x10,0x7C},//I
-//{0x3C,0x8,0x8,0x8,0x8,0x8,0x48,0x30},//J
-//{0x0,0x24,0x28,0x30,0x20,0x30,0x28,0x24},//K
-//{0x40,0x40,0x40,0x40,0x40,0x40,0x40,0x7C},//L
-//{0x81,0xC3,0xA5,0x99,0x81,0x81,0x81,0x81},//M
-//{0x0,0x42,0x62,0x52,0x4A,0x46,0x42,0x0},//N
-//{0x3C,0x42,0x42,0x42,0x42,0x42,0x42,0x3C},//O
-//{0x3C,0x22,0x22,0x22,0x3C,0x20,0x20,0x20},//P
-//{0x1C,0x22,0x22,0x22,0x22,0x26,0x22,0x1D},//Q
-//{0x3C,0x22,0x22,0x22,0x3C,0x24,0x22,0x21},//R
-//{0x0,0x1E,0x20,0x20,0x3E,0x2,0x2,0x3C},//S
-//{0x0,0x3E,0x8,0x8,0x8,0x8,0x8,0x8},//T
-//{0x42,0x42,0x42,0x42,0x42,0x42,0x22,0x1C},//U
-//{0x42,0x42,0x42,0x42,0x42,0x42,0x24,0x18},//V
-//{0x0,0x49,0x49,0x49,0x49,0x2A,0x1C,0x0},//W
-//{0x0,0x41,0x22,0x14,0x8,0x14,0x22,0x41},//X
-//{0x41,0x22,0x14,0x8,0x8,0x8,0x8,0x8},//Y
-//{0x0,0x7F,0x2,0x4,0x8,0x10,0x20,0x7F},//Z
-
-
 void write_byte(uint8_t byte)
-{		
-	
+{			
 		for(int i=0;i<8;i++)
 		{
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);  // Pull the CLK LOW
 			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, byte&0x80);// Write one BIT data MSB first
 			byte = byte<<1;  // shift the data to the left
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 1);  // Pull the CLK HIGH		
-		 }
-			
+		 }			
 }
 
-void write_max (uint8_t address, uint8_t data)
+void write_matrix (uint8_t address, uint8_t data)
 {
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);   // Pull the CS pin LOW
-    write_byte(address);   //  write address
-    write_byte(data);  //  write data
+    write_byte(address);  										 //  write address
+    write_byte(data);												  //  write data
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 1);  // pull the CS HIGH
 }
 
-void max_init(void)
+void matrix_init(void)
 {
-		write_max(0x09, 0x00);       //  no decoding
-		write_max(0x0a, 0x03);       //  brightness intensity
-		write_max(0x0b, 0x07);       //  scan limit = 8 LEDs
-		write_max(0x0c, 0x01);       //  power down =0,normal mode = 1
-		write_max(0x0f, 0x00);       //  no test display
+		write_matrix(0x09, 0x00);       //  no decoding
+		write_matrix(0x0a, 0x03);       //  brightness intensity
+		write_matrix(0x0b, 0x07);       //  scan limit = 8 LEDs
+		write_matrix(0x0c, 0x01);       //  power down =0,normal mode = 1
+		write_matrix(0x0f, 0x00);       //  no test display
 }
 
-void write_string (char *str)
+void display_right(void)
 {
-	while (*str)
-	{
-		for(int i=1;i<9;i++)
-			   {
-			       write_max (i,disp1ay[(*str - 55)][i-1]);
-			   }
-		(*str)++;
-		HAL_Delay (500);
-	}
+	for (int j=2; j<4; j++)
+		{			
+			write_matrix (1 , disp1ay[j][0]);
+			HAL_Delay (20);
+			write_matrix (8 , disp1ay[j][7]);			
+			HAL_Delay (20);
+			write_matrix (2 , disp1ay[j][1]);
+			HAL_Delay (20);
+			write_matrix (7 , disp1ay[j][6]);
+			HAL_Delay (20);
+			write_matrix (3 , disp1ay[j][2]);
+			HAL_Delay (20);
+			write_matrix (6 , disp1ay[j][5]);
+			HAL_Delay (20);
+			write_matrix (4 , disp1ay[j][3]);
+			HAL_Delay (20);
+			write_matrix (5 , disp1ay[j][4]);
+			HAL_Delay (520);
+		}
 }
-/* USER CODE END 0 */
+
+void display_left(void)
+{		
+		for (int j=0; j<2; j++)
+		{			
+			write_matrix (1 , disp1ay[j][0]);
+			HAL_Delay (20);
+			write_matrix (8 , disp1ay[j][7]);			
+			HAL_Delay (20);
+			write_matrix (2 , disp1ay[j][1]);
+			HAL_Delay (20);
+			write_matrix (7 , disp1ay[j][6]);
+			HAL_Delay (20);
+			write_matrix (3 , disp1ay[j][2]);
+			HAL_Delay (20);
+			write_matrix (6 , disp1ay[j][5]);
+			HAL_Delay (20);
+			write_matrix (4 , disp1ay[j][3]);
+			HAL_Delay (20);
+			write_matrix (5 , disp1ay[j][4]);
+			HAL_Delay (520);
+		}
+}
 
 /**
   * @brief  The application entry point.
@@ -158,51 +109,15 @@ void write_string (char *str)
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-	max_init ();
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	matrix_init ();
   while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-		for (int j=0; j<13; j++)
-		{
-			for (int i=1; i<9; i++)
-			{
-				write_max (i , disp1ay[j][i-1]);
-			}
-			HAL_Delay (500);
-		}
-		//write_string ("Hello");
-	//	HAL_Delay (1000);
+  {		
+		display_left();
+		display_right();
   }
-  /* USER CODE END 3 */
 }
 
 /**
@@ -222,7 +137,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL5;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
   RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -233,7 +148,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV4;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
